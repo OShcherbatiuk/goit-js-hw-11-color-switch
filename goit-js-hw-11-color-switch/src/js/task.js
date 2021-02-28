efa6d3a16abdfe03e485cc412c1a7a3ced80b279
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-02-28 20:27:00 +0200

    upd

--- goit-js-hw-11-color-switch/src/js/task.js
+++ goit-js-hw-11-color-switch/src/js/task.js
@@ -10,3 +10,27 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const startBtnRef = document.querySelector('[data-action ="start"]');
+const stopBtnRef = document.querySelector('[data-action ="stop"]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', onStartClick);
+stopBtnRef.addEventListener('click', onStopClick);
+
+let interval = null;
+
+function onStartClick({target}) {
+  target.disabled = true;
+  interval = setInterval(randomColor, 1000)
+};
+
+function randomColor() {
+  const bgColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  bodyRef.style.backgroundColor = bgColor;
+}
+
+function onStopClick() {
+  startBtnRef.disabled = false;
+  clearInterval(interval)
+};
